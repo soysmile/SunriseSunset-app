8513f400dc465b3f22bec91391fabf8fcd688acb
George <soysmile@outlook.com>
2018-05-18 13:09:02 +0300

    app added

---
+++ SunriseSunset app/CheckInternetConnection.swift
@@ -0,0 +1,9 @@
+//
+//  CheckInternetConnection.swift
+//  SunriseSunset app
+//
+//  Created by George Heints on 18.05.2018.
+//  Copyright © 2018 George Heints. All rights reserved.
+//
+
+import Foundation
